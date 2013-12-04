c8a68bf6c73e6258bf3708a3c3d97d937623e085
paulinarubio <Paulina@Paulinas-MacBook-Pro.local>
2013-12-04 11:44:45 -0800

    Initial Commit

---
+++ remove/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  remove
+//
+//  Created by Paulina Rubio Tarriba on 12/4/13.
+//  Copyright (c) 2013 Paulina Rubio Tarriba. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
